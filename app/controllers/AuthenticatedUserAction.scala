4b58cc334191aa4085298d6f33343d1c95f1b6b2
Giap Nguyen Ba <giap_nb@septeni-technology.jp>
2020-03-20 13:31:33 +0700

    -- complete !!!

--- app/controllers/AuthenticatedUserAction.scala
+++ app/controllers/AuthenticatedUserAction.scala
@@ -15,10 +15,10 @@ class AuthenticatedUserAction @Inject() (parser: BodyParsers.Default)(implicit e
   private val SESSION_USERNAME_KEY = "username"
 
   override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
-    logger.info("ENTERED AuthenticatedUserAction::invokeBlock ...")
     val maybeUsername = request.session.get(SESSION_USERNAME_KEY)
     maybeUsername match {
       case None => {
+        println(">>>>>.vao ayyy.....")
         Future.successful(
           Results.Redirect("/login")
         )
